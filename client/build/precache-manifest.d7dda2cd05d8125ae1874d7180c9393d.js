7b7cccaa6109159136c49284ca528fe6c13bb68f
NathanARob <NathanARob@users.noreply.github.com>
2018-10-05 10:54:43 +0200

    added build

---
+++ client/build/precache-manifest.d7dda2cd05d8125ae1874d7180c9393d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ab4066acc35d42a3aed48bacb817e89",
+    "url": "/static/media/logo.7ab4066a.jpg"
+  },
+  {
+    "revision": "2066997b7168ea4dd36f2fb0861c112f",
+    "url": "/static/media/banner.2066997b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a69f36a2d37771e3bd9",
+    "url": "/static/js/main.5a69f36a.chunk.js"
+  },
+  {
+    "revision": "dd5ce5b402db546d309a",
+    "url": "/static/js/1.dd5ce5b4.chunk.js"
+  },
+  {
+    "revision": "5a69f36a2d37771e3bd9",
+    "url": "/static/css/main.c2c6477f.chunk.css"
+  },
+  {
+    "revision": "d48427add6355ea2c85929f18609822a",
+    "url": "/index.html"
+  }
+];
